27c21ae1932e15335fbb0400b1e6eb8e953cdfa8
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-31 21:27:00 +0530

    update

--- pk.py
+++ pk.py
@@ -1,1 +1,13 @@
-print("hello world")
+numbers = [10, 5, 20, 8, 15]
+
+largest = numbers[0]
+second = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        second = largest
+        largest = num
+    elif num > second and num != largest:
+        second = num
+
+print("Second Largest:", second)
